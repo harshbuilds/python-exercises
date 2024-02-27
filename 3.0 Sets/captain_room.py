e77eaa335aef753ae77a8730a2ec5592ffe26fe9
testing <109216639+harshbuilds@users.noreply.github.com>
2024-02-27 20:58:19 +0530

    map returns an iterator

--- 3.0 Sets/captain_room.py
+++ 3.0 Sets/captain_room.py
@@ -1,11 +1,10 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
 n = input()
-arr = map(int, input().split())
+arr = list(map(int, input().split()))
 s = set(arr)
 k = set()
 for i in arr:
     if i in k:
-        s.remove(i)
+        s.discard(i)
     else:
         k.add(i)
 print(s.pop())
